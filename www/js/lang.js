1a3073630e3accd39d3b360d2884cc166f582d55
wonliao <wonliao1117@gmail.com>
2016-09-08 23:31:43 +0900

    Project's Initial Commit

---
+++ www/js/lang.js
@@ -0,0 +1,168 @@
+// This is a JavaScript file
+
+var en = {
+    
+    // index.html
+    index_menu_itme_1:"Home",
+    index_menu_itme_2:"About    ",
+    index_menu_itme_3:"Products",
+    index_menu_itme_4:"Contact",
+    index_version:"Version",
+
+    // page1.html
+    page1_item_1:"Profile",
+    page1_item_2:"Introduction",
+    page1_item_3:"Catalog",
+    
+    // page2-1.html
+    page2_item_1:"Plastics",
+    page2_item_2:"Rubbers",
+    page2_item_3:"Electronic Materials",
+    page2_item_4:"Specialty Chemicals",
+    
+    // page2-2.html
+    page2_2_item_1:"Brand",
+    page2_2_item_2:"Grade",
+    page2_2_item_3:"Place",
+    page2_2_item_4:"Industry",
+    page2_2_item_5:"Color",
+    page2_2_item_6:"OR",
+    page2_2_item_7:"Clear search",
+    page2_2_item_8:"Search",
+    search_text:"Product Name",
+    search_result:"No results found.",
+    
+    // page2-3.html
+    page2_3_item_1:"",
+    page2_3_item_2:"item found",
+    
+    // page3.html
+    page3_item_1:"Contact us",
+    page3_item_2:"Official website",
+    page3_item_3:"Contactor",
+    page3_item_4:"Headquarters & Rende Site 71702",
+    page3_item_5:"No.59-1, Sanjia, Rende Dist., Tainan City 71702, Taiwan",
+    page3_item_6:"Rende Site 71702",
+    page3_item_7:"No.59-1, Sanjia, Rende Dist., Tainan City 71702, Taiwan",
+    page3_item_8:"Tree Valley Site 74148",
+    page3_item_9:"No.18, Wangjia Rd., Xinshi Dist., Tainan City 74148, Taiwan",
+    page3_item_10:"Tokyo Office",
+    page3_item_11:"Nansei-Yaesu Bldg. 2F, 2-2-10 Yaesu, Chuo-ku, Tokyo",
+    page3_item_12:"Zhenjiang Chi Mei Chemical Co., Ltd.",
+    page3_item_13:"No.18, Hanfeng Rd., Dagang Zhenjiang New Area, Jiangsu Province, China",
+    page3_item_14:"Everywhere in Your Life",
+    page3_item_15:"Chi Mei",
+    page3_item_16:"CMC at a glance",
+    page3_item_17:"CMC was founded by Mr. Shi Wen-Long in 1960 with a company headquarters located in Tainan, Taiwan, and was Taiwan's first producer of acrylic panel and ABS resins. Chi Mei extended its reach overseas in 1996 with the establishment of Zhenjiang Chimei Chemical. The group's consolidated revenue from petrochemicals was NT$151 billion in 2014, and employees numbered approximately 3,400. The group's chief production sites are located in Tainan, Taiwan and Zhenjiang in China's Jiangsu Province. Overall annual production capacity of all products reached 3.5 million tons, including an annual capacity of 2.1 million tons of ABS resins and 340,000 tons of PMMA resins.",
+    page3_item_18:"Best Partner",
+    page3_item_19:"Chi Mei Corp.'s products have a very wide range of applications, and are used in countless everyday consumer goods and many industrial products. In order to satisfy the far-ranging needs of the market and our customers, we listen to customers carefully, and harness our top-notch R&D equipment, vast production capacity, high efficiency, environmentally-friendly production technologies, and an exacting quality management system to provide high-quality, cost-competitive products, helping customers to get a jump on their competitors.",
+    page3_item_20:"Making Life More Convenient",
+    page3_item_21:"Looking ahead to the future, we have established new materials research departments dedicated to investigation and development of new materials. We are realizing our vision of sustainable operation, enhancing added value for customers, making life more convenient, improving people's quality of life, and contributing to a brighter future.",
+
+    // home.html
+    home_main_text:"bonding your moments"
+};
+
+var zh = {
+    
+    // index.html
+    index_menu_itme_1:"首頁",
+    index_menu_itme_2:"關於奇美",
+    index_menu_itme_3:"產品搜尋",
+    index_menu_itme_4:"聯絡我們",
+    index_version:"版本",
+    
+    // page1.html
+    page1_item_1:"公司簡介",
+    page1_item_2:"九大產業介紹",
+    page1_item_3:"產品總型錄",
+    
+    // page2-1.html
+    page2_item_1:"塑膠系列",
+    page2_item_2:"橡膠系列",
+    page2_item_3:"電子材料",
+    page2_item_4:"複合材料",
+    
+    // page2-2.html
+    page2_2_item_1:"產品類別",
+    page2_2_item_2:"等級",
+    page2_2_item_3:"產地",
+    page2_2_item_4:"用途",
+    page2_2_item_5:"顏色",
+    page2_2_item_6:"或是",
+    page2_2_item_7:"清除搜尋條件",
+    page2_2_item_8:"搜尋",
+    search_text:"請輸入型號",
+    search_result:"沒有結果",
+    
+    // page2-3.html
+    page2_3_item_1:"搜尋結果，共 ",
+    page2_3_item_2:"樣產品",
+    
+    // page3.html
+    page3_item_1:"聯絡我們",
+    page3_item_2:"聯絡官網",
+    page3_item_3:"聯絡窗口",
+    page3_item_4:"總公司 71702",
+    page3_item_5:"台南市仁德區三甲里 59-1 號",
+    page3_item_6:"仁德廠區 71702",
+    page3_item_7:"台南市仁德區三甲里 59-1 號",
+    page3_item_8:"特用化學品樹谷廠區 74148",
+    page3_item_9:"台南市新市區王甲路 18 號",
+    page3_item_10:"東京聯絡事務所",
+    page3_item_11:"東京都中央区八重洲 2-2-10 南星八重洲ビル 2F",
+    page3_item_12:"鎮江奇美化工有限公司",
+    page3_item_13:"江蘇省鎮江大港開發區韓峰路 18 號",
+    page3_item_14:"全球熱塑型塑料領導品牌",
+    page3_item_15:"CHIMEI 奇美實業",
+    page3_item_16:"厚實根基 值得信賴",
+    page3_item_17:"CHIMEI 奇美實業於 1960 年由許文龍先生創立,總部位於台灣台南,為全 球主要的綜合塑料優質供應商,目前 ABS 樹脂、PMMA 樹脂及導光板世界 產能第一,近年更躍升為電子材料供應大廠。奇美原物料半世紀來,不斷被 廣泛應用於生活中各式用品的加工製造,生活隨處可見。",
+    page3_item_18:"環保先驅 塑造潮流",
+    page3_item_19:"透過先進的研發架構、高效率產能,以及全面品質管控流程,我們積極開發 更符合世界潮流與潔淨環境的先進材料,致力貼心滿足客戶各種不同需求, 並與全球客戶攜手守護地球與人類健康,並獲取絕佳競爭優勢。",
+    page3_item_20:"創新應用 前瞻未來",
+    page3_item_21:"奇美是全球第一家符合食品接觸塑料最嚴苛要求的供應商。我們的創新腳步 從不停歇,半世紀以來努力不懈的研發新原料,近年更投入高值化材料應用, 以期為人們創造更高品質、更美好愉悅的便利生活。",
+
+    // home.html
+    home_main_text:"精彩时刻 源自奇美"
+};
+
+
+
+
+
+
+
+function Language(lang)
+{
+    var __construct = function() {
+        if (eval('typeof ' + lang) == 'undefined')
+        {
+            lang = "en";
+        }
+        return;
+    }()
+
+    this.getStr = function(str, defaultStr) {
+        var retStr = eval('eval(lang).' + str);
+        if (typeof retStr != 'undefined')
+        {
+            return retStr;
+        } else {
+            if (typeof defaultStr != 'undefined')
+            {
+                return defaultStr;
+            } else {
+                return eval('en.' + str);
+            }
+        }
+    }
+}
+
+
+
+var language = navigator.language || navigator.userLanguage; 
+var codes = language.toString().split("-");
+var gLang = codes[0];
+//alert("gLang("+gLang+")");
+//gLang = "en";
+var translator = new Language(gLang);
