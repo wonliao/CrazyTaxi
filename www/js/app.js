f9b0a45f5e6dfb80495f24772a30d886c104807b
wonliao <wonliao1117@gmail.com>
2016-09-13 23:34:42 +0900

    test

--- www/js/app.js
+++ www/js/app.js
@@ -61,7 +61,42 @@ function statusChangeCallback(response) {
 // code below.
 function checkLoginState() {
     FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
+        //statusChangeCallback(response);
+        
+    if (!response.session) {
+        //initiate FB OAuth js popup dialog
+        FB.ui({
+            method: 'oauth',
+            display: 'page',
+            scope: 'email',
+            perms: 'email'
+        },
+        function(response) {
+            if (response.session) { //if permission Allowed
+                var thesession = response.session;
+                var thesession = eval('(' + thesession + ')'); //decode json
+                //POSTing to local file get.user_graph.php, to fetch user info
+                $.ajax({
+                    type: "POST",
+                    url: "get.user_graph.php",
+                    data: "client_id=<?php echo $appId; ?>&client_secret=<?php echo $secret; ?>&sessions="+thesession.session_key+"&type=client_cred&uid="+thesession.uid,
+                    dataType: "text",
+                    success: function(user_graph){
+                        var user_graph1 = eval('('+user_graph+')');
+                        alert(user_graph1.name); //users name
+                        alert(user_graph1.id); //users id
+                        alert(user_graph1.email); //users email
+                        alert(user_graph1.link); //users profile link
+                    }
+                });
+            } else {
+                //if permission Not Allowed
+            }
+        });
+    }
+        
+        
+        
     });
 }
 
