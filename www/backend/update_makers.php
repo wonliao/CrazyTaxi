12d4ba65ba7c5e12113f85016026ad6e734ca6bb
won-livebricks <won.liao@livebricks.com>
2016-11-30 20:39:45 +0800

    test

--- www/backend/update_makers.php
+++ www/backend/update_makers.php
@@ -32,13 +32,13 @@ foreach($makers as $key => $maker) {
 			$time = date("H:i", $time);
 
 			$to = $area->email;
-			$subject = "刊登成功通知！- go2gether大車隊共乘";
+			$subject = "您的共乘需求時間已到期通知！- go2gether大車隊共乘";
 			$body = "$area->fb_name 小姐/先生<br />
 			<br />
 			感謝您使用go2gether大車隊共乘。<br />
-			您的刊登需求已經成功上架於網站首頁囉！<br />
+			您的刊登需求已經到期，網站首頁將不再顯示該筆刊登囉！<br />
 			<br />
-			刊登詳情如下：<br />
+			已到期的刊登詳情如下：<br />
 			上車地點：$area->input_address<br />
 			下車地點：$area->destination<br />
 			出發日期：$date<br />
@@ -49,17 +49,11 @@ foreach($makers as $key => $maker) {
 			if($area->email != "") { $body .= "電子信箱：$area->email<br />";	}
 			
 			$body .= "<br />
-			當您收到此封系統通知信件後，將可於共乘網站的首頁，找到您的共乘需求圖標（顯示為黃色圖標）。<br />
+			為了提供您更優質的共乘體驗，希望您對於此次共乘網站使用的經驗，<br />
+			有任何建議或意見提供，請分享給我們！<br />
+			go2gether大車隊共乘 使用體驗分享<br />
+			<a href='https://goo.gl/forms/DWJF2ekv9BdWvy663'>https://goo.gl/forms/DWJF2ekv9BdWvy663</a><br />
 			<br />
-			在等候夥伴聯繫您的同時，您也可進行以下動作：<br />
-			1. 資訊確認：回到共乘網站首頁，確認你的刊登資訊是否正確。<br />
-			2. 夥伴聯繫：隨時注意來自夥伴的 電話/簡訊/email 聯繫訊息。<br />
-			3. 查詢記錄：點選右上角側邊欄，選擇「刊登紀錄」，查看所有刊登過的需求。<br />
-			4. 準時出發：當共乘夥伴主動聯繫並約好碰面後，請記得準時到達預定的上車地點。<br />
-			5. 再次刊登：把下次要共乘的需求，前往完成吧！<br />
-			   ＊go2gether大車隊共乘：<a href='https://goo.gl/gje0G6'>https://goo.gl/gje0G6</a><br />
-			6. 提前於手機下載55688APP，讓您出門更加便利！<br />
-			   ＊下載教學：<a href='http://app.taiwantaxi.com.tw/APPS/Mobile/Intro.aspx'>http://app.taiwantaxi.com.tw/APPS/Mobile/Intro.aspx</a><br />
 			<br />
 			<br />
 			如果您有任何疑問，請隨時聯繫我們。<br />
