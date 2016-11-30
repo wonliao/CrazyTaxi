7daecc949a196f685634dca24028b9aeb57057dd
won-livebricks <won.liao@livebricks.com>
2016-11-30 13:18:42 +0800

    test

---
+++ www/backend/send_mail.php
@@ -0,0 +1,36 @@
+<?php
+require 'PHPMailer/PHPMailerAutoload.php';
+
+//sendMail("wonliao1117@gmail.com", "共乘通知", "測試內文");
+
+function sendMail($to, $subject, $body) {
+
+	$mail= new PHPMailer(); //建立新物件        
+	$mail->IsSMTP(); //設定使用SMTP方式寄信        
+	$mail->SMTPAuth = true; //設定SMTP需要驗證        
+	$mail->SMTPSecure = "ssl"; // Gmail的SMTP主機需要使用SSL連線   
+	$mail->Host = "smtp.gmail.com"; //Gamil的SMTP主機        
+	$mail->Port = 465;  //Gamil的SMTP主機的SMTP埠位為465埠。        
+	$mail->CharSet = "UTF-8"; //設定郵件編碼        
+	
+	$mail->Username = "go2gether2016@gmail.com"; //設定驗證帳號        
+	$mail->Password = "go2gether55688"; //設定驗證密碼        
+	
+	$mail->From = "go2gether2016@gmail.com"; //設定寄件者信箱        
+	$mail->FromName = "go2gether大車隊共乘"; //設定寄件者姓名        
+	
+	$mail->Subject = $subject; // "共乘通知";
+	
+	//$body = "共乘出發";//'测试邮件,附上中文乱码解决方法:' . "<br>" .'123'; 
+	$mail->MsgHTML( $body );
+	
+	$mail->IsHTML(true); //設定郵件內容為HTML        
+	$mail->AddAddress($to, "親愛的用戶"); //設定收件者郵件及名稱        
+	
+	if(!$mail->Send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+		echo "Message sent!";
+	}
+}
+?>
--- www/backend/update_makers.php
+++ www/backend/update_makers.php
@@ -3,32 +3,34 @@
 ini_set("display_errors", true);
 require_once("firebase/firebaseLib.php");
 
+require_once("send_mail.php");
+
 const DEFAULT_URL = 'https://go2gether-e78d4.firebaseio.com/';
 const DEFAULT_TOKEN = 'FEDPrbRuQnPmgA8cPyprs4YRGKpityaBDaS6dNB5';
 
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
+$makers = json_decode( $firebase->get("makers") );
+foreach($makers as $key => $maker) {
 
-$areas = json_decode( $firebase->get("areas") );
-//var_dump($areas);
+	$_area = $firebase->get( 'areas/'. $key );
+	$area = json_decode($_area);
 
-
-$current_time = time()."000";
-//echo "current_time($current_time)<br />";
-
-foreach($areas as $key => $value) {
-
-	$expired_time = $value->expired_time;
-	//echo "key($key) address(".$value->address.") expired_time($expired_time)<br />";
-	
+	$expired_time = $area->expired_time;
 	// 刊登過期，移除 makers
 	if($current_time > $expired_time) {
+
+		// 寄送 email
+		if($area->email == "wonliao1117@gmail.com") {
+
+			$email = $area->email;
+			$subject = "共乘通知";
+			$body = "共乘已結束";
+			sendMail($email, $subject, $body);
+		}
 
 		//echo "key($key) address(".$value->address.") expired_time($expired_time)<br />";
 		$firebase->delete("makers/$key");
 	}
 }
-
-
-
 ?>
