fcdb8e60869cba05650918a713a8ee3b2fd4f384
won-livebricks <won.liao@livebricks.com>
2016-12-01 16:50:08 +0800

    test

--- www/backend/update_makers.php
+++ www/backend/update_makers.php
@@ -25,46 +25,43 @@ foreach($makers as $key => $maker) {
 	if($current_time > $expired_time) {
 
 		// 寄送 email
-		if($area->email == "wonliao1117@gmail.com") {
+		$time = floor($area->appointment / 1000);
+		$date = date("Y-m-d", $time);
+		$time = date("H:i", $time);
 
-			$time = floor($area->appointment / 1000);
-			$date = date("Y-m-d", $time);
-			$time = date("H:i", $time);
+		$to = $area->email;
+		$subject = "您的共乘需求時間已到期通知！- go2gether大車隊共乘";
+		$body = "$area->fb_name 小姐/先生<br />
+		<br />
+		感謝您使用go2gether大車隊共乘。<br />
+		您的刊登需求已經到期，網站首頁將不再顯示該筆刊登囉！<br />
+		<br />
+		已到期的刊登詳情如下：<br />
+		上車地點：$area->input_address<br />
+		下車地點：$area->destination<br />
+		出發日期：$date<br />
+		出發時間：$time<br />";
+		
+		if($area->note != "") { $body .= "備註：$area->note<br />";	}
+		if($area->phones != "") { $body .= "手機號碼：$area->phones<br />";	}
+		if($area->email != "") { $body .= "電子信箱：$area->email<br />";	}
+		
+		$body .= "<br />
+		<font color='red'>為了提供您更優質的共乘體驗，希望您對於此次共乘網站使用的經驗，</font><br />
+		<font color='red'>有任何建議或意見提供，請分享給我們！</font><br />
+		<font color='red'>go2gether大車隊共乘 使用體驗分享：</font><br />
+		<a href='https://goo.gl/forms/DWJF2ekv9BdWvy663'>https://goo.gl/forms/DWJF2ekv9BdWvy663</a><br />
+		<br />
+		<br />
+		<br />
+		如果您有任何疑問，請隨時聯繫我們。<br />
+		感謝您使用go2gether大車隊共乘<br />
+		活動網站：<a href='https://goo.gl/gje0G6'>https://goo.gl/gje0G6</a><br />
+		官方粉絲團：<a href='https://goo.gl/cVIwMp'>https://goo.gl/cVIwMp</a><br />
+		<br />
+		";
 
-			$to = $area->email;
-			$subject = "您的共乘需求時間已到期通知！- go2gether大車隊共乘";
-			$body = "$area->fb_name 小姐/先生<br />
-			<br />
-			感謝您使用go2gether大車隊共乘。<br />
-			您的刊登需求已經到期，網站首頁將不再顯示該筆刊登囉！<br />
-			<br />
-			已到期的刊登詳情如下：<br />
-			上車地點：$area->input_address<br />
-			下車地點：$area->destination<br />
-			出發日期：$date<br />
-			出發時間：$time<br />";
-			
-			if($area->note != "") { $body .= "備註：$area->note<br />";	}
-			if($area->phones != "") { $body .= "手機號碼：$area->phones<br />";	}
-			if($area->email != "") { $body .= "電子信箱：$area->email<br />";	}
-			
-			$body .= "<br />
-			<font color='red'>為了提供您更優質的共乘體驗，希望您對於此次共乘網站使用的經驗，</font><br />
-			<font color='red'>有任何建議或意見提供，請分享給我們！</font><br />
-			<font color='red'>go2gether大車隊共乘 使用體驗分享：</font><br />
-			<a href='https://goo.gl/forms/DWJF2ekv9BdWvy663'>https://goo.gl/forms/DWJF2ekv9BdWvy663</a><br />
-			<br />
-			<br />
-			<br />
-			如果您有任何疑問，請隨時聯繫我們。<br />
-			感謝您使用go2gether大車隊共乘<br />
-			活動網站：<a href='https://goo.gl/gje0G6'>https://goo.gl/gje0G6</a><br />
-			官方粉絲團：<a href='https://goo.gl/cVIwMp'>https://goo.gl/cVIwMp</a><br />
-			<br />
-			";
-
-			sendMail($to, $subject, $body);
-		}
+		sendMail($to, $subject, $body);
 
 		//echo "key($key) address(".$value->address.") expired_time($expired_time)<br />";
 		$firebase->delete("makers/$key");
