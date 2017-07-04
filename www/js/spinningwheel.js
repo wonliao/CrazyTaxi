2b01c15d79388aad46fbcda3ce4fb0fbe0107e53
wonliao <wonliao1117@gmail.com>
2017-07-05 01:20:52 +0900

    test

--- www/js/spinningwheel.js
+++ www/js/spinningwheel.js
@@ -146,6 +146,12 @@
 
 		lockScreen: function (e) {
 			//e.preventDefault();
+            if (event.cancelable) {
+                // 判断默认行为是否已经被禁用
+                if (!event.defaultPrevented) {
+                    event.preventDefault();
+                }
+            }
 			e.stopPropagation();
 		},
 
