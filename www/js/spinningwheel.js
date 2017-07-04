b1787ea46b1e3ee619b035da5d69405fe39ca6ec
wonliao <wonliao1117@gmail.com>
2017-07-05 01:47:46 +0900

    test

--- www/js/spinningwheel.js
+++ www/js/spinningwheel.js
@@ -144,15 +144,13 @@
 			this.swWrapper.style.top = (this.container ? this.container.clientHeight : (window.innerHeight + window.pageYOffset)) + 'px';
 		},
 
-		lockScreen: function (evt) {
+		lockScreen: function (e) {
            
             if(getMobileOperatingSystem() != "Android") {
                 e.preventDefault();
             }
 
-			evt.stopPropagation();
-            
-            return false;
+			e.stopPropagation();
 		},
 
 
@@ -623,6 +621,26 @@
 		}
 	};
 	
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    
+          // Windows Phone must come first because its UA also contains "Android"
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+    
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+    
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+    
+        return "unknown";
+    }
+
 	function prefixStyle (style) {
 		if ( vendor === '' ) return style;
 
